7e1fc0b4d09e2b47ce6383993ea22e5744780df1
sandeshdubeyy <sandeshdubeyy@gmail.com>
2026-06-14 21:22:07 +0530

    feat: Add Expense model with schema definition for tracking expenses

---
+++ server/src/models/Expense.models.ts
@@ -0,0 +1,60 @@
+import mongoose,{Schema, Document} from "mongoose";
+
+export interface IExpense extends Document{
+    title:string;
+    amount:number;
+    category:string;
+    type:"income" | "expense";
+    paymentMethod:string;
+    date:Date;
+    isReccuring:boolean;
+    note:string;
+    user:mongoose.Types.ObjectId;
+}
+
+const expenseSchema = new Schema<IExpense>(
+    {
+        title:{
+            type:String,
+            required:true,
+        },
+        amount:{
+            type:Number,
+            required:true,
+        },
+        category:{
+            type:String,
+            required:true,
+        },
+        type:{
+            type:String,
+            enum:["income","expense"],
+            required:true,
+        },
+        paymentMethod:{
+            type:String,
+            enum:["upi","cash","bank_transfer","card"],
+            required:true,
+        },
+        date:{
+            type:Date,
+            default:Date.now,
+        },
+        isReccuring:{
+            type:Boolean,
+            default:false,
+        },
+        note:{
+            type:String,
+        },
+        user:{
+            type:mongoose.Types.ObjectId,
+            required:true,
+        },
+    },
+    {
+        timestamps:true,
+    }
+)
+
+export default mongoose.model<IExpense>("Expense",expenseSchema);
